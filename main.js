301762247ba7175837c9a0e0e330abcbcd4ac70b
Universe-protector <84567831+Universe-protector@users.noreply.github.com>
2022-09-20 18:50:23 +1000

    Update main.js

--- main.js
+++ main.js
@@ -19,9 +19,7 @@ function take_snapshot()
 }
 
   console.log('ml5 version:', ml5.version);
-  
-  // Initialize the Image Classifier method with MobileNet
-//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+ 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/j1mpM3G8y/model.json',modelLoaded);
 
   // When the model is loaded
